7e792dff67ca0a774d83a779c899a81d380ec372
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-03 09:57:25 +1300

    commented

--- math.js
+++ math.js
@@ -1,9 +1,11 @@
-var nOne = 10;
-var nTwo = 20;
-var nThree = 30;
-var nFour = 40;
+var nOne = 10; //creates a variable called "nOne" and sets it to the value of '10'
+var nTwo = 20; //creates a variable called "nTwo" and sets it to the value of '20'
+var nThree = 30; //creates a variable called "nThree" and sets it to the value of '30'
+var nFour = 40; //creates a variable called "nFour" and sets it to the value of '40'
 
-var nCalc = nOne / nTwo + nThree / nOne - nFour % nFour;
+var nCalc = nOne / nTwo + nThree / nOne - nFour % nFour; 
+// creates a variable of the equation we want to answer so that we dont have to keep writing it out
 
-console.log("Result of nOne / nTwo + nThree / nOne - nFour % nFour");
-console.log(nCalc);
+console.log("Result of nOne / nTwo + nThree / nOne - nFour % nFour"); // prints to the console the equation we want to answer
+console.log(nCalc);//Prints to the console the numerical value of 'nCalc'
+
