c80e3e52cbe53c25ac5e6d90860661e42f40acac
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-03 09:26:23 +1300

    Test_2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-var test1;
+var nOne = 50;
+var nTwo = 10;
+var sum = nOne + nTwo;
+var subtract = nOne - nTwo;
+var divide = nOne / nTwo;
+var percent = 50 % 10;
+var multiply = nOne * nTwo;
+
+console.log("sum of nOne and nTwo");
+console.log(sum);
+
+console.log("subtract nTwo from nOne");
+console.log(subtract);
+
+console.log("divide nOne by nTwo");
+console.log(divide);
+
+console.log("Remainder of nOne divided by nTwo");
+console.log(percent);
+
+console.log("nOne multiplied by nTwo");
+console.log(multiply);
+
