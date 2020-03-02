c32433061f05a872712f512c94eaf084a3aa55a4
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-03 09:43:36 +1300

    commented

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var nOne = 50;
-var nTwo = 10;
-var sum = nOne + nTwo;
-var subtract = nOne - nTwo;
-var divide = nOne / nTwo;
-var percent = nOne % nTwo;
-var multiply = nOne * nTwo;
+var nOne = 50; //Creates a variable "nOne" and sets it to '50' that can be used throughout the code
+var nTwo = 10;  //Creates a variable "nTwo" and sets it to '10' that can be used throughout the code
+var sum = nOne + nTwo; //Creates a variable called "sum" which is the sum of the declared variables
+var subtract = nOne - nTwo; //Creates a variable called "subtract" which is the output of one variable subtracted from the other
+var divide = nOne / nTwo; //Creates a variable called "divide" which is the product of one variable divided by the other
+var percent = nOne % nTwo; //Creates a variable called "percent" which is the remainder of the one variable diveded by the other
+var multiply = nOne * nTwo; //Creates a variable called "multiply" which is the product of one variable multiplied by the other
+ 
+console.log("sum of nOne and nTwo");// prints to the console the equation we want to answer
+console.log(sum); //prints to the console the value of the variable 
 
-console.log("sum of nOne and nTwo");
-console.log(sum);
+console.log("subtract nTwo from nOne");// prints to the console the equation we want to answer
+console.log(subtract); //prints to the console the value of the variable
 
-console.log("subtract nTwo from nOne");
-console.log(subtract);
+console.log("divide nOne by nTwo");// prints to the console the equation we want to answer
+console.log(divide); //prints to the console the value of the variable
 
-console.log("divide nOne by nTwo");
-console.log(divide);
+console.log("Remainder of nOne divided by nTwo");// prints to the console the equation we want to answer
+console.log(percent); //prints to the console the value of the variable
 
-console.log("Remainder of nOne divided by nTwo");
-console.log(percent);
-
-console.log("nOne multiplied by nTwo");
-console.log(multiply);
+console.log("nOne multiplied by nTwo");// prints to the console the equation we want to answer
+console.log(multiply); //prints to the console the value of the variable
