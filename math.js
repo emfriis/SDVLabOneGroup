f7039ff40d26d1c6e238559437f2c3d953135928
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-03 09:52:43 +1300

    math.js

---
+++ math.js
@@ -0,0 +1,9 @@
+var nOne = 10;
+var nTwo = 20;
+var nThree = 30;
+var nFour = 40;
+
+var nCalc = nOne / nTwo + nThree / nOne - nFour % nFour;
+
+console.log("Result of nOne / nTwo + nThree / nOne - nFour % nFour");
+console.log(nCalc);
